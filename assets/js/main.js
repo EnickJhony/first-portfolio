856a80608b98df77709b3af4cb57e0ed3014a047
Enik Jhony <enick.jhony@gmail.com>
2022-07-06 23:07:59 -0400

    finalizando o Services para mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,40 +19,38 @@ if (navClose) {
 /* =========================== REMOVE MENU MOBILE =========================== */
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
 
 /* =========================== ACCORDION SKILLS =========================== */
 
 const skillsContent = document.getElementsByClassName('skills__content'),
-skillsHeader = document.querySelectorAll('.skills__header')
+  skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
+function toggleSkills() {
   let itemClass = this.parentNode.className
 
-  for ( i=0 ; i < skillsContent.length; i++){
+  for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = 'skills__content skills__close'
   }
-  if(itemClass === 'skills__content skills__close'){
+  if (itemClass === 'skills__content skills__close') {
     this.parentNode.className = 'skills__content skills__open'
   }
 }
 
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach(el => {
   el.addEventListener('click', toggleSkills)
 })
 
 /* =========================== QUALIFICATION TABS =========================== */
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+  tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab =>{
-  tab.addEventListener('click', () =>{
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.target)
 
     tabContents.forEach(tabContent => {
@@ -60,11 +58,33 @@ tabs.forEach(tab =>{
     })
     target.classList.add('qualification__active')
 
-    tabs.forEach(tab =>{
+    tabs.forEach(tab => {
       tab.classList.remove('qualification__active')
     })
     tab.classList.add('qualification__active')
   })
 })
 
+/* =========================== SERVICES MODAL =========================== */
+const modalViews = document.querySelectorAll('.services__modal'),
+  modalBtns = document.querySelectorAll('.services__button'),
+  modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(1)
+  })
+})
+
+modalCloses.forEach(modalClose => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach(modalView => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
 /* =========================== GOOGLE FONTS =========================== */
